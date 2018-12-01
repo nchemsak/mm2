9b44227fc1d2ba56023d20bd2751695af38d4c01
Nick <nchemsak@gmail.com>
2018-12-01 17:44:52 -0600

    initial

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// EYE JS
+"use strict";
+
+// $(document).ready(function() {
+
+//     // image info
+//     // width: 1850px / 5 = 370 columns
+//     // height: 1000px / 5 = 200 rows
+//     // 74000 to divs
+
+
+// });
+
+
+
+
+
+// $(document).ready(function() {
+//     for (var i = 1; i <= 1; i++) {
+//         // $('#target').append('<div class="dot-rows" id="dot-row-' + i + '">');
+//         for (var j = 1; j <= 18500; j++) {
+//             $('#target').append('<div class="all-dots" id="dot-' + j + '"></div>')
+
+//             // if (j % 370 === 0) {
+//             //     $('#target').append('<br />')
+//             // }
+//         }
+//         // $('#target').append('</ div>' );
+//     }
+// });
