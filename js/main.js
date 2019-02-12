7376f961ba138247d99eb29e46ca27145918aae0
Nick <nchemsak@gmail.com>
2019-02-12 15:03:33 -0600

    update

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@
 var counterNorth = 49;
 var counterEast = 48;
 var counterEastMain = 165;
-
-// Open SIDE OPTIONS MENU when letter 'O' is pressed on keyboard
 var direction = "north";
 
 // Change the counter and background-position of bee when leaving the shop
@@ -108,9 +106,9 @@ $(document).keydown(function(e) {
                 $(".main-bee1").css("left", (counterEastMain) + "px");
                 $(".main-bee2").css("left", (counterEastMain) + "px");
             }
-    if (window.location.href.indexOf("recipe-book") > -1) {
-    console.log("recipe shopppp");
-}
+            if (window.location.href.indexOf("recipe-book") > -1) {
+                console.log("recipe shopppp");
+            }
 
             if (counterEastMain === 550 && window.location.href.indexOf("recipe-book") > -1) {
                 $(".header-image").append('<div id="recipe-popup" class="popover fade show bs-popover-top" role="tooltip" x-placement="top"><div class="arrow" style="right: 26px;"></div><div class="popover-body">Hi, Welcome to my shop! <br /> What recipe would you like to learn? <br />Please select a recipe from the menu on the right.</div></div>');
@@ -198,42 +196,61 @@ $(document).keydown(function(e) {
             // if (counterEastMain === 0 && window.location.href.indexOf("katsura") > -1) {
             //             window.location.href = "index.html?left-tree";
             //         }
-
-
-
-
         }
 
         $(".direction").attr("class", "direction west");
     }
 
     // GO TO LINK WHEN AT SPECIFIC LOCATION
-    // console.log("counterEast:", counterEast);
-    // console.log("counterNorth:", counterNorth);
-    if (counterEast === 48 && counterNorth === 77) {
-        console.log("Welcome to the Restaurant!");
-        window.location.href = "recipe-book.html";
-    }
-
-    if (counterEast === 13 && counterNorth === 63) {
-        console.log("Welcome to the tree!");
-        window.location.href = "katsura.html";
-    }
 
     console.log("counterEast", counterEast);
     console.log("counterNorth", counterNorth);
 
+    // middlegate - recipes
+    if (counterEast === 48 && counterNorth === 77) {
+        window.location.href = "/recipe-book";
+    }
+    // tree - Katsura
+    if (counterEast === 13 && counterNorth === 63) {
+        window.location.href = "/katsura";
+    }
+    // top right castle - coding
     if (counterEast === 90 && counterNorth === -7) {
-        console.log("Welcome to the castle!");
-        window.location.href = "coding.html";
+        window.location.href = "/coding";
     }
-
-
+    // drum machine
+    if (counterEast === 55 && counterNorth === 28) {
+        window.location.href = "/drum-machine";
+    }
+    // house - character
     if (counterEast === 90 && counterNorth === 42) {
-        console.log("Welcome home!");
-        window.location.href = "character.html";
+        window.location.href = "/character";
+    }
+    // big white castle - art
+    if (counterEast === 27 && counterNorth === 7) {
+        window.location.href = "/art";
+    }
+    // bottom castle - rss
+    if (counterEast === 76 && counterNorth === 91) {
+        window.location.href = "/rss";
+    }
+    // cave - contact
+    if (counterEast === 13 && counterNorth === 21) {
+        window.location.href = "/contact";
     }
 
+    // Sprial
+    if (counterEast === 62 && counterNorth === 56) {
+        // window.location.href = "/contact";
+        console.log("spriral!");
+        $("body").addClass("flip");
+    }
+
+    if (counterEast != 62 || counterNorth != 56) {
+        // window.location.href = "/contact";
+        console.log("no spriral!");
+        $("body").removeClass("flip");
+    }
 
     // var mainBeeCounter = 5;
     // if (event === 39) {
@@ -282,7 +299,8 @@ function recipeShopVisit() {
 }
 
 
-// sound
+
+// ATTACK button
 var play = function() {
     var audio = document.getElementById("audio");
     audio.play("/audio/slap.mp3");
@@ -296,3 +314,107 @@ $('.seizure').on('click', function(e) {
     }, millisecondsToWait);
 
 });
+
+
+
+/********************************************
+            theremin code
+  **********************************************/
+var SynthPad = (function() {
+    // Variables
+    var myCanvas;
+    var frequencyLabel;
+    var volumeLabel;
+    var myAudioContext;
+    var oscillator;
+    var gainNode;
+
+    // Notes
+    var lowNote = 5;
+    var highNote = 60;
+
+    // Constructor
+    var SynthPad = function() {
+        myCanvas = document.getElementById('thereminCanvas');
+
+        // Create an audio context.
+        myAudioContext = new AudioContext();
+
+        SynthPad.setupEventListeners();
+    };
+
+    // Event Listeners
+    SynthPad.setupEventListeners = function() {
+
+        // Disables scrolling on touch devices.
+        document.body.addEventListener('touchmove', function(event) {
+            event.preventDefault();
+        }, false);
+
+        myCanvas.addEventListener('mousedown', SynthPad.playSound);
+        myCanvas.addEventListener('touchstart', SynthPad.playSound);
+        myCanvas.addEventListener('mouseup', SynthPad.stopSound);
+        document.addEventListener('mouseleave', SynthPad.stopSound);
+        myCanvas.addEventListener('touchend', SynthPad.stopSound);
+    };
+
+    // Play a note.
+    SynthPad.playSound = function(event) {
+        oscillator = myAudioContext.createOscillator();
+        gainNode = myAudioContext.createGain();
+        oscillator.type = 'square';
+        gainNode.connect(myAudioContext.destination);
+        oscillator.connect(gainNode);
+        SynthPad.updateFrequency(event);
+        oscillator.start(0);
+        myCanvas.addEventListener('mousemove', SynthPad.updateFrequency);
+        myCanvas.addEventListener('touchmove', SynthPad.updateFrequency);
+        myCanvas.addEventListener('mouseout', SynthPad.stopSound);
+    };
+
+    // Stop the audio.
+    SynthPad.stopSound = function(event) {
+        oscillator.stop(0);
+        myCanvas.removeEventListener('mousemove', SynthPad.updateFrequency);
+        myCanvas.removeEventListener('touchmove', SynthPad.updateFrequency);
+        myCanvas.removeEventListener('mouseout', SynthPad.stopSound);
+    };
+
+    // Calculate the note frequency.
+    SynthPad.calculateNote = function(posX) {
+        var noteDifference = highNote - lowNote;
+        var noteOffset = (noteDifference / myCanvas.offsetWidth) * (posX - myCanvas.offsetLeft);
+        return lowNote + noteOffset;
+    };
+
+    // Calculate the volume.
+    SynthPad.calculateVolume = function(posY) {
+        var volumeLevel = 1 - (((100 / myCanvas.offsetHeight) * (posY - myCanvas.offsetTop)) / 100);
+        return volumeLevel;
+    };
+
+    // Get the new frequency and volume.
+    SynthPad.calculateFrequency = function(x, y) {
+        var noteValue = SynthPad.calculateNote(x);
+        // console.log("noteValue: ", noteValue);
+        var volumeValue = SynthPad.calculateVolume(y);
+
+        oscillator.frequency.value = noteValue;
+        gainNode.gain.value = volumeValue;
+    };
+
+    // Update the note frequency.
+    SynthPad.updateFrequency = function(event) {
+        if (event.type == 'mousedown' || event.type == 'mousemove') {
+            SynthPad.calculateFrequency(event.x, event.y);
+        } else if (event.type == 'touchstart' || event.type == 'touchmove') {
+            var touch = event.touches[0];
+            SynthPad.calculateFrequency(touch.pageX, touch.pageY);
+        }
+    };
+    // Export SynthPad.
+    return SynthPad;
+
+})();
+var synthPad = new SynthPad();
+
